019b0675e18ff451020183a2840de8378e591a26
hagghat <mahach0396@gmail.com>
2024-05-14 02:52:40 +0600

    my job data update

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000
 
@@ -39,14 +39,56 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
 
     const jobsCollection = client.db('mahach0396').collection('jobs')
+   
 
     // get all jobs
 
     app.get("/jobs", async (req, res) => {
-        const result = await jobsCollection.find().toArray()
+        const result = await jobsCollection.find({}).toArray()
 
         res.send(result)
     })
+
+    // get single data
+
+    app.get('/job/:id', async (req, res) =>{
+       const id = req.params.id
+       const query = {_id : new ObjectId(id)}
+       const result = await jobsCollection.findOne(query)
+       res.send(result)
+    })
+
+    // my job data
+
+
+    app.get('/jobs', async(req,res) =>{
+      try{
+        const Email = req.query.userEmail;
+        // console.log(Email);
+        const query = {userEmail:Email};
+        const myJobsByEmail = await jobsCollection.find(query).toArray();
+        res.json(myJobsByEmail)
+      }catch (error){
+        console.log('Error fatching data', error);
+        res.status(500).json({error:'Internal server error'})
+      }
+    }
+  )
+
+
+
+    // add data saving 
+
+
+    app.post('/addJobs', async(req, res) =>{
+      const addData = req.body
+      const result = await jobsCollection.insertOne(addData)
+      console.log(result);
+      res.send(result)
+    })
+
+
+
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
